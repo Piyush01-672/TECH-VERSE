fadf1bfa2e64c05172758379577f5a3cba6b9467
Piyush Sharma <piyushsharma8280@gmail.com>
2026-04-18 14:11:40 +0530

    done

--- Frontend/src/pages/CodeCrafter.tsx
+++ Frontend/src/pages/CodeCrafter.tsx
@@ -141,7 +141,7 @@ const CodeCrafter = () => {
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const gatewayOpenDate = new Date("2026-03-20T00:00:00");
-  const eventCloseDate = new Date("2026-04-19T23:59:59");
+  const eventCloseDate = new Date("2026-04-18T18:00:00");
   const [isGatewayOpen, setIsGatewayOpen] = useState(new Date() >= gatewayOpenDate);
 
   useEffect(() => {
